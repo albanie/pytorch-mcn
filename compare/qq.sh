6cbc75753755eba5393805b519f9462418382b0b
albanie <albanie@robots.ox.ac.uk>
2017-12-29 10:08:53 +0000

    refactor

--- compare/qq.sh
+++
@@ -1,4 +0,0 @@
-mcn_import_dir="~/data/models/matconvnet"
-output_dir="~/data/models/pytorch/mcn_imports"
-feat_dir="~/data/pt/pytorch-mcn/feats"
-ipython compare_models.py -- $mcn_import_dir/inception_v3-pt-mcn.mat $output_dir $feat_dir
